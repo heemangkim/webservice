d999ab42d3083bbf488755da26bb0fd05a46e9d0
hmkim <hmkim@smartscore.kr>
2021-03-29 15:58:52 +0900

    login api

--- src/main/java/com/webservice/web/MyHomeController.java
+++ src/main/java/com/webservice/web/MyHomeController.java
@@ -3,6 +3,8 @@ package com.webservice.web;
 import com.webservice.model.req.RequestLogin;
 import com.webservice.service.MyHomeService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequiredArgsConstructor
 public class MyHomeController {
+    private static final Logger logger = LoggerFactory.getLogger(MyHomeController.class);
 
     private final MyHomeService myHomeService;
 
-    @PostMapping("/loginAction")
+    @PostMapping("/v1/hm/login")
     public Object login(HttpServletRequest request, HttpServletResponse response, @RequestBody RequestLogin body) {
-
+        logger.info("[CONTROLLER] -> login()");
         return myHomeService.loginAction(request, response, body).getResponse();
     }
 }
